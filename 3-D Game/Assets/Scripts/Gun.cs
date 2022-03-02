0d205b193b6990a3797de50964462e98de6af157
SMdG <72032559+Surya-mdg@users.noreply.github.com>
2022-03-02 22:16:04 +0530

    Added Basic Gun System
    Added gun shooting and object damage mechanism.

---
+++ 3-D Game/Assets/Scripts/Gun.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Refrences")]
+    [SerializeField] GunData gun;
+    [SerializeField] Transform cam;
+
+    float timeSinceLastShot = 0f;
+    float currentAmmo;
+
+    private void Start()
+    {
+        PlayerShoot.shootInput += Shoot;
+        PlayerShoot.reloadInput += StartReload;
+
+        currentAmmo = gun.currentAmmo;
+    }
+
+    private void StartReload()
+    {
+        if (!gun.reloading)
+        {
+            StartCoroutine("Reload");
+        }
+    }
+
+    private IEnumerator Reload()
+    {
+        Debug.Log("Reloading");
+
+        gun.reloading = true;
+        yield return new WaitForSeconds(gun.reloadTime);
+
+        currentAmmo = gun.magSize;
+        gun.reloading = false;
+    }
+
+    private bool CanShoot() => !gun.reloading && timeSinceLastShot >= (1f / (gun.fireRate / 60f));
+
+    private void Shoot()
+    {
+        Debug.Log("Shot Gun");
+        if (currentAmmo > 0)
+        {
+            if (CanShoot())
+            {
+                if(Physics.Raycast(cam.position, cam.forward, out RaycastHit hitInfo, gun.maxDistance))
+                {
+                    Debug.Log(hitInfo.transform.name);
+
+                    IDamageble damageble = hitInfo.transform.GetComponent<IDamageble>();
+                    damageble?.Damage(gun.damage);
+                }
+
+                currentAmmo--;
+                timeSinceLastShot = 0f;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        timeSinceLastShot += Time.deltaTime;
+
+        Debug.DrawRay(cam.position, cam.forward * gun.maxDistance);
+    }
+}
